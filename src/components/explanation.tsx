f7d1b0dd88f6ed06f3c4bf58d15c2a8385aa2839
AlexSnipes <alejandro.sanchez.trek@gmail.com>
2024-04-21 11:22:44 -0300

    Fix 3

--- src/components/explanation.tsx
+++ src/components/explanation.tsx
@@ -18,20 +18,18 @@ export default function Explanation() {
         {isa.temperatureGradient}°C por cada 1000 metros o 1.98°C por cada 1000 pies, hasta la tropopausa que se
         mantiene constante en -56.6°C. Por lo tanto la temperatura del aire a {isa.getAltitude().toFeet().toFixed(2)}{' '}
         pies se obtendrá de la siguiente manera:
-        <div className="text-center text-sm font-bold my-2">
-          t = T<sub>0</sub> + (a * h)/1000
-        </div>
       </p>
-      <p>
-        <ul>
-          <li>t = Temperatura del aire a {altitude.toFixed(2)} pies</li>
-          <li>
-            T<sub>0</sub> = Temperatura estándar al nivel del mar ({isa.standardTemperature}°C)
-          </li>
-          <li>a = Gradiente térmico</li>
-          <li>h = Altitud en metros</li>
-        </ul>
-      </p>
+      <div className="text-center text-sm font-bold my-2">
+        t = T<sub>0</sub> + (a * h)/1000
+      </div>
+      <ul>
+        <li>t = Temperatura del aire a {isa.getAltitude().toFeet().toFixed(2)} pies</li>
+        <li>
+          T<sub>0</sub> = Temperatura estándar al nivel del mar ({isa.standardTemperature}°C)
+        </li>
+        <li>a = Gradiente térmico</li>
+        <li>h = Altitud en metros</li>
+      </ul>
       <div className="text-center text-sm font-bold my-2">
         t = 15 + (-6.5 * {isa.getAltitude().toMeters().toFixed(2)})/1000 = {isa.calculateTemperature().toCelsius()}°C
       </div>
